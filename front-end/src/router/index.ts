cfed669d35820fb974790b4337072d01337f390d
Sara Bevilacqua <sarabev6@gmail.com>
2023-11-12 16:12:25 +0100

    fix: router linked to correct view

--- front-end/src/router/index.ts
+++ front-end/src/router/index.ts
@@ -1,8 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import SaveMyNotesView from '../views/SaveMyNotesView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: SaveMyNotesView,
+    }
+  ]
 })
 
 export default router
