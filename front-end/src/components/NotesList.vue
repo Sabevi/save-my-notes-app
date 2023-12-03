4654f98d34adf83d5227e511dffa087dce0b484a
Sara Bevilacqua <sarabev6@gmail.com>
2023-12-03 16:22:32 +0100

    fix: changed content of NotesList component

--- front-end/src/components/NotesList.vue
+++ front-end/src/components/NotesList.vue
@@ -1,64 +1,32 @@
 <template>
-  <div>
-    <h1>Notes</h1>
-    <AddNote :note="selectedNote" @noteAdded="fetchNotes" @noteUpdated="fetchNotes" />
-    <ul>
-      <li v-for="note in notes" :key="note.id">
-        {{ note.note }}
-        <UpdateNote :id="note.id" @update-note="triggerUpdate" />
-        <DeleteNote :id="note.id" @noteRemoved="fetchNotes" />
-      </li>
-    </ul>
-  </div>
+  <ul>
+    <li v-for="note in notes" :key="note.id">
+      {{ note.note }}
+      <UpdateNote :id="note.id" @update-note="$emit('update-note', note.id)" />
+      <DeleteNote :id="note.id" @noteRemoved="$emit('noteRemoved')" />
+    </li>
+  </ul>
 </template>
 
 <script lang="ts">
-import axios from "redaxios";
-import { ref, type Ref } from "vue";
-import AddNote from "./AddNote.vue";
 import DeleteNote from "./DeleteNote.vue";
 import UpdateNote from "./UpdateNote.vue";
 
-// see where to put types
-type Note = {
+interface Note {
   id: number;
   note: string;
-};
-
-const note: Ref<Note> = ref({
-  id: 0,
-  note: "",
-});
+}
 
 export default {
+  props: {
+    notes: {
+      type: Array as () => Note[],
+      required: true,
+    },
+  },
   components: {
-    AddNote,
     DeleteNote,
     UpdateNote,
-  },
-  data() {
-    return {
-      notes: [] as Note[],
-      selectedNote: null as Note | null,
-    };
-  },
-  methods: {
-    fetchNotes() {
-      axios
-      .get(`${import.meta.env.VITE_VUE_APP_API_URL}`)
-        .then((response) => {
-          this.notes = response.data;
-        })
-        .catch((error) => {
-          console.error("Error:", error);
-        });
-    },
-    triggerUpdate(id: number) {
-      this.selectedNote = this.notes.find(note => note.id === id) || null;
-    },
-  },
-  mounted() {
-    this.fetchNotes();
   },
 };
 </script>
--- front-end/src/views/SaveMyNotesView.vue
+++ front-end/src/views/SaveMyNotesView.vue
@@ -1,12 +1,56 @@
-<script setup lang="ts">
-import AddNote from "@/components/AddNote.vue";
-import NotesList from "@/components/NotesList.vue";
-</script>
-
 <template>
-  <main>
-    <h1>Save my Notes</h1>
-    <br />
-    <NotesList />
-  </main>
+  <div>
+    <h1>Notes</h1>
+    <AddNote :note="selectedNote" @noteAdded="fetchNotes" @noteUpdated="fetchNotes" />
+    <NotesList :notes="notes" @update-note="triggerUpdate" @noteRemoved="fetchNotes" />
+  </div>
 </template>
+
+<script lang="ts">
+import axios from "redaxios";
+import { ref, type Ref } from "vue";
+import AddNote from "../components/AddNote.vue";
+import NotesList from "../components/NotesList.vue";
+
+// see where to put types
+type Note = {
+  id: number;
+  note: string;
+};
+
+const note: Ref<Note> = ref({
+  id: 0,
+  note: "",
+});
+
+export default {
+  components: {
+    AddNote,
+    NotesList,
+  },
+  data() {
+    return {
+      notes: [] as Note[],
+      selectedNote: null as Note | null,
+    };
+  },
+  methods: {
+    fetchNotes() {
+      axios
+      .get(`${import.meta.env.VITE_VUE_APP_API_URL}`)
+        .then((response) => {
+          this.notes = response.data;
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+        });
+    },
+    triggerUpdate(id: number) {
+      this.selectedNote = this.notes.find(note => note.id === id) || null;
+    },
+  },
+  mounted() {
+    this.fetchNotes();
+  },
+};
+</script>
